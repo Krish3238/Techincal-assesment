02392ea0e0251eeb4d39e464d5ad13dd8fe6cc49
Krish3238 <106793231+Krish3238@users.noreply.github.com>
2024-07-16 21:24:07 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  description = "The ID of the GCP project"
+}
+
+variable "region" {
+  description = "The region to deploy the application"
+  default     = "us-central"
+}
